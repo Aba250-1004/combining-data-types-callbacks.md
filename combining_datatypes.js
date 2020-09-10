a6e28fe71768700a081b8d7f5226bf71d3515dac
Amal Amchtal <amchtala@union.edu>
2020-09-10 14:22:00 -0400

    Combine objects, arrays, and functions more than one level deep

--- combining_datatypes.js
+++ combining_datatypes.js
@@ -48,3 +48,44 @@ let apartmentBuilding = [
     ["amal","taylor","mateen"]
 ] 
 console.log(apartmentBuilding[0][1]);
+
+// Create a function knit that returns an object that 
+// has the following kinds of properties item: 
+// scarf, size : 6ft etc. Log a value of that 
+// object (hint: call the function and then call a
+// property on the return value).
+const knit = () => {
+    let toRet = {
+        isScarf:true,
+        size: "6ft",
+    }
+    return toRet;
+}
+
+console.log(knit().isScarf)
+ 
+// Create a function crayonSelector that returns an
+//  object that has an array (you can reuse your crayonBox 
+// object). Log one of the elements of that array.
+const crayonSelector = () => {
+    var crayonBox = {
+        crayons: ["blue","yellow","purple"],
+    }
+    return crayonBox;
+}
+
+console.log(crayonSelector().crayons[1])
+
+// Create a function powerButton that returns a 
+// function called options - options should 
+// console.log a simple message like select a 
+// song. Call that inner function
+const powerButton = (callback) => {
+    return callback()
+}
+
+const options = () =>{
+    console.log("select a song")
+}
+
+powerButton(options);
